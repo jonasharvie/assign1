88047cf94487d2ecb2cd553b641613fb48490213
jonasharvie <134028039+jonasharvie@users.noreply.github.com>
2025-09-28 22:40:24 -0400

    update home and about pages, delete unnecessary files

--- src/About.jsx
+++ src/About.jsx
@@ -1,27 +1,20 @@
 
-import ShopFront from './images/Centennial_Ramen_Shop_Front.png'
-
+import Jonas from './images/Jonas.jpg'
 
 function About(){
     return(
         <>
         <article>
             <h2 class="stars">About Me</h2>
-
             <div class="image-text-row">
-            <img src={ShopFront} alt="" width="300" />
-            <div class="text-content">
-                <p>
-                We are dedicated to serving high-quality, affordable, and
-                lightning-fast ramen bowls that fuel students, faculty, and staff
-                through their busy days. With a focus on fresh ingredients, friendly
-                service, and a fun, welcoming atmosphere.
-                </p>
-            </div>
-            </div>
-
-            
-            <br />
+                <img src={Jonas} alt="" width="300" />
+                <div class="text-content">
+                    <p>
+                    I am a mechanical designer working on quality automation processes in the aerospace industry. 
+                    I am enrolled in Centennial College's software engineering technology A.I program to apply what I learn to the aerospace industry.
+                    </p>
+                </div>
+            </div> 
             <br />
         </article>
         <br />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
-import CentennialRamenLogo from './images/Centennial_Ramen_Logo3V2.png'
+
 import star from './images/star.png'
-import ShopFront from './images/Centennial_Ramen_Shop_Front.png'
+import Jonas from './images/Jonas.jpg'
 
 
 function Home(){
@@ -10,69 +10,17 @@ function Home(){
             <h2 class="stars">Home</h2>
 
             <div class="image-text-row">
-            <img src={ShopFront} alt="" width="300" />
+            <img src={Jonas} alt="" width="300" />
             <div class="text-content">
+                <h2 class="stars">Mission Statement</h2>
                 <p>
-                We are dedicated to serving high-quality, affordable, and
-                lightning-fast ramen bowls that fuel students, faculty, and staff
-                through their busy days. With a focus on fresh ingredients, friendly
-                service, and a fun, welcoming atmosphere.
+                Mechanical designer with focus on developing automation processes in the aerospace industry. 
+                My goal is to gain more knowledge if software and A.I development and apply what I learn to the aerospace industry.
                 </p>
             </div>
             </div>
 
-            <br />
-            <hr />
-            <nav class="stars">
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            </nav>
-            <nav>
-            <b class="paragraph">
-                The spicy miso is amazing, and the portions are perfect for the
-                student budget. Highly recommend!
-                <br />
-                We couldn’t be happier with their service!
-            </b>
-            <b class="paragraph"> - Fiona and Shrek </b>
-            </nav>
-
-            <hr />
-            <nav class="stars">
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            </nav>
-            <nav>
-            <b class="paragraph">
-                Centannial Ramen makes campus life so much better.
-                <br />
-                It's the perfect place to hang out with friends or grab a bowl to go.
-                <br />Can't wait to try every flavor!
-            </b>
-            <b class="paragraph"> - Nick Fury </b>
-            </nav>
-            <hr />
-            <nav class="stars">
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            <img src={star} width="20" height="20" alt="star" />
-            </nav>
-            <nav>
-            <b class="paragraph">
-                Centannial Ramen is my go-to spot for a quick, satisfying meal.
-                <br />
-            </b>
-            <b class="paragraph"> - Ed </b>
-            </nav>
-            <br />
+            
             <br />
         </article>
         <br />
